75e587285645377962dd7cefe4d73eef21954dff
Mizu <silicumcarbid@proton.me>
2023-02-23 10:33:33 +0100

    Erste Version + Updated ReadMe

--- README.md
+++ README.md
@@ -1,5 +1,30 @@
 # technick
 Bei Technik fragen Tech Nick fragen!
 
+# Funktionen
+### Momentan gibt es 3 Funktionen 
+> /technick
+Schickt ein zufälliges Element aus der Liste "technick". Momentan befinden sich dort nur Werbevideos.
+> /abgenickt
+Entscheidet zufällig ob etwas "angenickt" oder "angepisst" von Technik ist
+> /techfakt
+Schickt eine zufällige Zeile aus der Datei "fakt.txt". Dort befinden sich momentan Tech Fakten die ich durch ChatGPT habe. Es kann sein dass sich Fakten wiederholen.
+
 # Dependencies
-Der Bot nutzt [Pycord](https://pycord.dev/).
+
+Der Bot benötigt Python 3. Außerdem nutzt er [Pycord](https://pycord.dev/). 
+> pip install py-cord
+
+# Setup
+Aus Sicherheitsgründen erwartet der Bot den Token in einer .env datei
+Füge in DEINDISCORDTOKEN den Token von deinen Discord Bot ein
+Beispiel unter Linux: 
+> echo ""TOKEN" = DEINDISCORDTOKEN" > .env
+
+Oder einfach in eine .env datei folgendes reinschreiben:
+> "TOKEN" = DEINDISCORDTOKEN
+
+Um den Bot zu starten:
+> python main.py
+Oder
+> python3 main.py
---
+++ main.py
@@ -0,0 +1,37 @@
+# Jegliche Videos/Bilder sind auf meinen discord server gespeichert (Alles aus cdn.discordapp.com). 
+# Das verhindert Traffic und macht vieles einfacher, wenn der Kanal auf dem die Videos drauf sind nicht mehr existiert heißt dass das alle Funktionen mit Videos/Bilder auch nicht mehr existieren
+
+
+import discord
+import os 
+import random
+from dotenv import load_dotenv
+
+load_dotenv() 
+bot = discord.Bot()
+
+technick = ["https://cdn.discordapp.com/attachments/1078032962706231316/1078040293884043294/4hHXi9z.mp4","https://cdn.discordapp.com/attachments/1078032962706231316/1078040311860826183/iEwMepU.mp4"]
+abgenickt = ["Abgenickt", "Angepisst"]
+speechbubble = ["https://media.discordapp.net/attachments/1078032962706231316/1078239280813592687/watermark.png?width=916&height=610", "https://media.discordapp.net/attachments/1078032962706231316/1078239378826088499/watermark.png?width=960&height=543", "https://media.discordapp.net/attachments/1078032962706231316/1078239432622219285/watermark.png?width=960&height=480", "https://media.discordapp.net/attachments/1078032962706231316/1078239499064188990/watermark.png?width=960&height=540", "https://media.discordapp.net/attachments/1078032962706231316/1078239562557554689/watermark.png", "https://media.discordapp.net/attachments/1078032962706231316/1078239602457989210/watermark.png?width=960&height=540", "https://media.discordapp.net/attachments/1078032962706231316/1078239730002571334/watermark.png?width=960&height=480", "https://media.discordapp.net/attachments/1078032962706231316/1078239815562182736/watermark.png"]
+
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Game(name="FIFA"))
+    print(f"{bot.user} ist online")
+
+@bot.slash_command(name = "technick", description = "Bei Technik fragen Tech-Nick fragen!")
+async def werbespot(ctx):
+    await ctx.respond(random.choice(technick))
+
+@bot.slash_command(name = "choice", description = "Ist diese Nachricht abgenickt oder angepisst von technik 🤔")
+async def auswahl(ctx):
+    await ctx.respond(random.choice(abgenickt) + " von Technik!")
+
+@bot.slash_command(name = "techfakt", description = "Tech Nick erzählt euch einen zufälligen Tech Fakt!")
+async def fakt(ctx):
+    await ctx.respond(random.choice(open("fakt.txt").readlines())) 
+
+
+
+bot.run(os.getenv('TOKEN')) 
