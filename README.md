6372ba6737267eee245eccb3d064f4920d31c338
Mizu <silicumcarbid@proton.me>
2023-02-23 11:46:43 +0100

    Sprechblasen hinzugefügt

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 
 Bei Technik fragen Tech Nick fragen!
 
+## Invite  
+
+https://discord.com/api/oauth2/authorize?client_id=1078253643142283264&permissions=2147510272&scope=bot%20applications.commands
+
 ## Funktionen
 
-#### Momentan gibt es 3 Funktionen
+#### Das sind die momentanen Funktionen
 
 /technick
 
@@ -16,7 +20,11 @@ Bei Technik fragen Tech Nick fragen!
 
 /techfakt
 
-> Schickt eine zufällige Zeile aus der Datei "fakt.txt". Dort befinden sich momentan Tech Fakten die ich durch ChatGPT habe. Es kann sein dass sich Fakten wiederholen.
+> Schickt eine zufällige Zeile aus der Datei "fakt.txt". Dort befinden sich momentan Tech Fakten die ich durch ChatGPThabe. Es kann sein dass sich Fakten wiederholen.
+
+Markierung
+
+> Wenn der Bot markiert wird schickt er eine Sprechblase aus der speechbubble Liste. Er löscht außerdem die Nachricht in der er markiert wurde.
 
 ## Dependencies
 
@@ -30,7 +38,7 @@ Außerdem nutzt er [Pycord](https://pycord.dev/).
 
 Aus Sicherheitsgründen erwartet der Bot den Token in einer .env datei
 Füge in DEINDISCORDTOKEN den Token von deinen Discord Bot ein
-Beispiel unter Linux:  
+Beispiel unter Linux:
 `$ echo ""TOKEN" = DEINDISCORDTOKEN" > .env`
 
 Oder einfach in eine .env datei folgendes reinschreiben:
--- main.py
+++ main.py
@@ -22,18 +22,28 @@ speechbubble = ["https://media.discordapp.net/attachments/1078032962706231316/10
 async def on_ready():
     await bot.change_presence(activity=discord.Game(name="FIFA")) # Zeigt dass der Bot "FIFA" spielt.
     print(f"{bot.user} ist online")
+
+@bot.event
+async def on_message(message: discord.message): # wenn der bot markiert wird schickt er ein element aus der speechbubble liste und löscht die ursprüngliche nachricht
+    if str(bot.user.id) in message.content:
+        await message.delete()
+        await message.channel.send(random.choice(speechbubble))
+        print("speechbubble wurde benutzt")
         
 @bot.slash_command(name = "technick", description = "Bei Technik fragen Tech-Nick fragen!") #/technick befehlt. Schickt ein zufälliges Element aus der technick liste.
 async def werbespot(ctx):
     await ctx.respond(random.choice(technick))
+    print("/technick wurde benutzt")
 
 @bot.slash_command(name = "entscheidung", description = "Ist diese Nachricht abgenickt oder angepisst von technik 🤔") # /entscheidung befehlt. Schickt ein zufälliges Element aus der technick liste.
 async def auswahl(ctx):
     await ctx.respond(random.choice(abgenickt) + " von Technik!")
+    print("/entscheidung wurde benutzt")
 
 @bot.slash_command(name = "techfakt", description = "Tech Nick erzählt euch einen zufälligen Tech Fakt!") # /techfakt befehlt. Schickt eine zufällige Zeile aus der Datei fakt.txt
 async def fakt(ctx):
     await ctx.respond(random.choice(open("fakt.txt").readlines())) 
+    print("/fakt wurde benutzt")
 
 
 bot.run(os.getenv('TOKEN')) 
