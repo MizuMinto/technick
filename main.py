6430d778b0a3fe54e88d0fbc0592c160eb880673
Mizu <silicumcarbid@proton.me>
2023-02-24 21:53:13 +0100

    Gratis Geld Download!

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import discord
 import os 
 import random
 from dotenv import load_dotenv
+from time import ctime
 
 
 load_dotenv() 
@@ -21,29 +22,34 @@ speechbubble = ["https://media.discordapp.net/attachments/1078032962706231316/10
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Game(name="FIFA")) # Zeigt dass der Bot "FIFA" spielt.
-    print(f"{bot.user} ist online")
+    print(ctime() + ": " f"{bot.user} ist online") 
 
 @bot.event
 async def on_message(message: discord.message): # wenn der bot markiert wird schickt er ein element aus der speechbubble liste und löscht die ursprüngliche nachricht
     if str(bot.user.id) in message.content:
         await message.delete()
         await message.channel.send(random.choice(speechbubble))
-        print("speechbubble wurde benutzt")
+        print(ctime() + ": " " speechbubble wurde benutzt")
         
-@bot.slash_command(name = "technick", description = "Bei Technik fragen Tech-Nick fragen!") #/technick befehlt. Schickt ein zufälliges Element aus der technick liste.
+@bot.slash_command(name = "technick", description = "Bei Technik fragen Tech-Nick fragen!") #/technick befehl. Schickt ein zufälliges Element aus der technick liste.
 async def werbespot(ctx):
     await ctx.respond(random.choice(technick))
-    print("/technick wurde benutzt")
+    print(ctime() + ": " "/technick wurde benutzt")
 
-@bot.slash_command(name = "entscheidung", description = "Ist diese Nachricht abgenickt oder angepisst von technik 🤔") # /entscheidung befehlt. Schickt ein zufälliges Element aus der technick liste.
+@bot.slash_command(name = "entscheidung", description = "Ist diese Nachricht abgenickt oder angepisst von technik 🤔") # /entscheidung befehl. Schickt ein zufälliges Element aus der technick liste.
 async def auswahl(ctx):
     await ctx.respond(random.choice(abgenickt) + " von Technik!")
-    print("/entscheidung wurde benutzt")
+    print(ctime() + ": " "/entscheidung wurde benutzt")
 
-@bot.slash_command(name = "techfakt", description = "Tech Nick erzählt euch einen zufälligen Tech Fakt!") # /techfakt befehlt. Schickt eine zufällige Zeile aus der Datei fakt.txt
+@bot.slash_command(name = "techfakt", description = "Tech Nick erzählt euch einen zufälligen Tech Fakt!") # /techfakt befehl. Schickt eine zufällige Zeile aus der Datei fakt.txt
 async def fakt(ctx):
     await ctx.respond(random.choice(open("fakt.txt").readlines())) 
-    print("/fakt wurde benutzt")
+    print(ctime() + ": " "/fakt wurde benutzt")
 
+@bot.slash_command(name = "geld", description = "Gratis Geld Download") # /geld befehl. gratis geld download
+async def geld(ctx):
+    await ctx.respond("https://cdn.shopify.com/s/files/1/0550/1383/4888/files/Gratis_Geld.jpg?v=1656120813&width=550")
+    await ctx.send("Gratis Geld Download!")
+    print(ctime() + ": " "/geld wurde benutzt")
 
 bot.run(os.getenv('TOKEN')) 
